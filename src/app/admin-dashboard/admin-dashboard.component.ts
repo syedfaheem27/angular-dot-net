972dd683bde17389b5fa33161a73b180a93016dc
Syed Faheem <syedfaheem2719@gmail.com>
2024-06-10 19:12:10 +0530

    Add dashboard

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+interface User {
+  username: string;
+  password: string;
+  role: string;
+}
+
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
+  styleUrls: ['./admin-dashboard.component.css'],
 })
 export class AdminDashboardComponent implements OnInit {
-
-  constructor() { }
+  user: User | null = null;
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    const user = localStorage.getItem('user');
 
+    if (user) this.user = JSON.parse(user);
+  }
 }
--- src/app/non-admin-dashboard/non-admin-dashboard.component.ts
+++ src/app/non-admin-dashboard/non-admin-dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+interface User {
+  username: string;
+  password: string;
+  role: string;
+}
+
 @Component({
   selector: 'app-non-admin-dashboard',
   templateUrl: './non-admin-dashboard.component.html',
-  styleUrls: ['./non-admin-dashboard.component.css']
+  styleUrls: ['./non-admin-dashboard.component.css'],
 })
 export class NonAdminDashboardComponent implements OnInit {
-
-  constructor() { }
+  user: User | null = null;
+  constructor() {}
 
   ngOnInit(): void {
+    const user = localStorage.getItem('user');
+
+    if (user) this.user = JSON.parse(user);
   }
 
 }
